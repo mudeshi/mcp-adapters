f512153115d2ce6edbab99c41119e07909200a49
Malay Udeshi <malay_udeshi@yahoo.com>
2026-01-28 19:39:22 -0500

    Refactor code structure for improved readability and maintainability

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
-def main():
+import os
+import asyncio
+from dotenv import load_dotenv
+
+load_dotenv()
+
+print(os.getenv("LANGSMITH_PROJECT"))
+
+async def main():
     print("Hello from mcp-adapters!")
 
 
 if __name__ == "__main__":
-    main()
+   asyncio.run(main())
+
+
